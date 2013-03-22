cb2b781d0653c14733923a21cb36a19fe17bd29b
Mike Michaelevich <mmichael0413@gmail.com>
2013-03-22 15:19:55 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbShanghai::Application.config.secret_token = '3b0d1e49810ee6d3abacdc75b3bdec2e510a409078fe838ff11f426942338b35d7eaa16f5358f40e36b61664c5310084a5a5ecfc6616aa0e96a34510846f681a'
