ec9259fffae52e1c73e4a6c42a32f84e5872c619
Mike Michaelevich <mmichael0413@gmail.com>
2013-03-25 16:34:53 -0400

    play around with deploy.rb for cap deployment to Windows Azure VM

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,16 +4,26 @@ set :repository,  "git@github.com:mmichael0413/fb-shanghai.git"
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `scm`, `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
+# set :location, "fb-shanghai.cloudapp.net"
+
 set :branch, "master"
 # to force cap to reuse single remote git clone instead of creating a new one with each 'cap deploy'
 set :deploy_via, :remote_cache
 
-# use 'server' instead of 'role' for a single server
-server "fb-shanghai.cloudapp.net", :app, :web, :db, :primary => true
+set :user, "mmichaelevich"
+set :use_sudo, false
 
-# role :web, "168.63.238.94:54065"                          # Your HTTP server, Apache/etc
-# role :app, "168.63.238.94:54065"                          # This may be the same as your `Web` server
-# role :db,  "fb-shanghai.cloudapp.net", :primary => true # This is where Rails migrations will run
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+
+# use 'server' instead of 'role' for a single server
+# server "fb-shanghai.cloudapp.net", :app, :web, :db, :primary => true
+
+# Also tried using roles instead of 'server' but that prompts a password that I can not get passed
+role :web, "168.63.237.104"                          # Your HTTP server, Apache/etc
+role :app, "168.63.237.104"                          # This may be the same as your `Web` server
+role :db, "168.63.237.104", :primary => true
+# role :db,  "v1o64h7a79.database.windows.net", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
