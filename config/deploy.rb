10efda85672639e8b19ff6caa595f3970a1fcd8e
Mike Michaelevich <mmichael0413@gmail.com>
2013-03-26 12:03:58 -0400

    add config files from RoR on Azure VM tutorial

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,45 +1,36 @@
+require "bundler/capistrano"
+ 
 set :application, "fb-shanghai"
-set :repository,  "git@github.com:mmichael0413/fb-shanghai.git"
-
-set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `scm`, `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-# set :location, "fb-shanghai.cloudapp.net"
-
-set :branch, "master"
-# to force cap to reuse single remote git clone instead of creating a new one with each 'cap deploy'
-set :deploy_via, :remote_cache
-
 set :user, "mmichaelevich"
-set :use_sudo, false
-
-ssh_options[:forward_agent] = true
+ 
+set :scm, :git
+set :repository, "git@github.com:mmichael0413/fb-shanghai.git"
+set :branch, "master"
+set :use_sudo, true
+ 
+server "168.63.237.104", :web, :app, :db, primary: true
+ 
+set :deploy_to, "/home/#{user}/apps/#{application}"
 default_run_options[:pty] = true
-
-# use 'server' instead of 'role' for a single server
-# server "fb-shanghai.cloudapp.net", :app, :web, :db, :primary => true
-
-# Also tried using roles instead of 'server' but that prompts a password that I can not get passed
-role :web, "168.63.237.104"                          # Your HTTP server, Apache/etc
-role :app, "168.63.237.104"                          # This may be the same as your `Web` server
-role :db, "168.63.237.104", :primary => true
-# role :db,  "v1o64h7a79.database.windows.net", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
-
-# if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
-
-# automatically run db migrations after each 'cap deploy'
-after "deploy:update_code", "deploy:migrate"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+ssh_options[:forward_agent] = true
+ 
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+ 
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    sudo "mkdir -p #{shared_path}/config"
+  end
+  after "deploy:setup", "deploy:setup_config"
+ 
+  task :symlink_config, roles: :app do
+    # Add database config here
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+end
