c71a030fe249fce53bc2d7ec4dae4b227518e6a1
Mike Michaelevich <mmichael0413@gmail.com>
2013-03-27 17:58:16 -0400

    change deprecated RAILS_ROOT to Rails.root in paperclip.rb initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -10,7 +10,7 @@ module Paperclip
         :whiny             => Paperclip.options[:whiny] || Paperclip.options[:whiny_thumbnails],
         :storage           => :azure1,
         :path              => ":modelname/:attachment/:id/:style/:filename",        
-        :azure_credentials => "#{RAILS_ROOT}/config/azure.yml",
+        :azure_credentials => "#{Rails.root}/config/azure.yml",
         :azure_container   => "system",
         :azure_host_alias  => "fbshanghai2013test1.blob.core.windows.net",
         :url               => ':azure_domain_url',
