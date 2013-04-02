91ede65f19b143c5a06c4796b38d42ed99be92e6
Mike Michaelevich <mmichael0413@gmail.com>
2013-04-02 16:57:33 -0400

    change deploy.rb for new VM

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,15 +6,13 @@ set :user, "mmichaelevich"
 set :scm, :git
 set :repository, "git@github.com:mmichael0413/fb-shanghai.git"
 set :branch, "master"
-set :use_sudo, false # use false for cap deploy:setup, every other time use true
+set :use_sudo, true
  
-server "168.63.249.187", :web, :app, :db, primary: true
-set :port, "58386"
+server "137.116.137.3", :web, :app, :db, primary: true
  
 set :deploy_to, "/home/#{user}/apps/#{application}"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true # uses local ssh keys for authentication with git repo
-#ssh_options[:keys] = ["/Users/mikemichaelevich/Programs/Fullbridge/myCert.pem"]
  
 #namespace :deploy do
 #  %w[start stop restart].each do |command|
